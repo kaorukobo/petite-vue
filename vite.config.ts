ff65adacd1a4a9e189f7b59cb1a8583d92327b98
Kaoru Shirai <475350+kaorukobo@users.noreply.github.com>
2025-03-07 17:14:31 +0900

    chore: add build:debug task to emit debug version (pico-vue.debug.TYPE.js)

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'node:path'
+import {defineConfig} from 'vite'
+import {resolve} from 'node:path'
+
+// Set DEBUG_BUILD=1 to build the debug version and sourcemaps
+const isDebug = process.env.DEBUG_BUILD === '1'
 
 export default defineConfig({
+  define: {
+    // NODE_ENV setting (required even for debug builds)
+    'process.env.NODE_ENV': JSON.stringify('production')
+  },
   build: {
     target: 'esnext',
-    minify: 'terser',
+    minify: isDebug ? false : 'terser',
+    sourcemap: isDebug,
+    // Don't remove previously done release build on debug build
+    emptyOutDir: !isDebug,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'PicoVue',
-      formats: ['es', 'umd', 'iife']
+      formats: ['es', 'umd', 'iife'],
+      fileName: (format) => `pico-vue.${format}${isDebug ? '.debug' : ''}.js`
     },
     rollupOptions: {
       plugins: [
