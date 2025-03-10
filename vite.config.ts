01063d6bc094da03099b74b12a926bf8da186d6d
Kaoru Shirai <475350+kaorukobo@users.noreply.github.com>
2025-03-10 10:13:27 +0900

    chore: use esbuild for minify instead of terser

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,15 @@ import {resolve} from 'node:path'
 // Set DEBUG_BUILD=1 to build the debug version and sourcemaps
 const isDebug = process.env.DEBUG_BUILD === '1'
 
-export default defineConfig({
+const config = defineConfig({
   define: {
     // NODE_ENV setting (required even for debug builds)
     'process.env.NODE_ENV': JSON.stringify('production')
   },
   build: {
     target: 'esnext',
-    minify: isDebug ? false : 'terser',
+    // 'terser' does not work for *.es.js build?
+    minify: isDebug ? false : 'esbuild',
     sourcemap: isDebug,
     // Don't remove previously done release build on debug build
     emptyOutDir: !isDebug,
@@ -35,5 +36,10 @@ export default defineConfig({
         }
       ]
     }
+
   }
 })
+
+console.log(JSON.stringify(config));
+
+export default config
